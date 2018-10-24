51adfd8381e1f6e431fd1d8baa67e875f6ec156b
zhaojiajia <641088582@qq.com>
2018-10-24 17:05:04 +0800

    更新之后的上传

---
+++ edu-before-client/src/main/java/com/jk/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.jk.service;
+
+        import com.jk.mapper.UserMapper;
+        import com.jk.modelapi.UserBean;
+        import com.jk.util.StringUtil;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
+
+/**
+ * @Auther: 赵黎明
+ * @Date: 2018/10/21 11:55
+ * 描述: 用一段话来简述
+ */
+@Service
+public class UserServiceImpl {
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public UserBean getUserByLoginName(UserBean userBean) {
+        return userMapper.getUserByLoginName(userBean);
+    }
+
+    public UserBean getUserphoneName(String phone) {
+
+        return userMapper.getUserphoneName(phone);
+    }
+
+    public void addUser(UserBean userBean) {
+        String uuId = StringUtil.getUUId();
+        userBean.setId(uuId);
+        userMapper.addUser(userBean);
+    }
+}
