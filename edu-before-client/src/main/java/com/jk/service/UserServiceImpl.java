0e53a56ec0df959741d647c619def2a84388b958
caonma <w320811803.@qq.com>
2018-10-25 20:07:19 +0800

    再次改动（id）

--- edu-before-client/src/main/java/com/jk/service/UserServiceImpl.java
+++ edu-before-client/src/main/java/com/jk/service/UserServiceImpl.java
@@ -27,8 +27,7 @@ public class UserServiceImpl {
     }
 
     public void addUser(UserBean userBean) {
-        String uuId = StringUtil.getUUId();
-        userBean.setId(uuId);
+
         userMapper.addUser(userBean);
     }
 }
