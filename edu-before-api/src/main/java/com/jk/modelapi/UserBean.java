58586a0af52e7ba9e11ebe7eb2b1863dfb9ea1c6
zlm <31626337@qq.com>
2018-10-25 15:34:04 +0800

    前台第er次本地上传

--- edu-before-api/src/main/java/com/jk/modelapi/UserBean.java
+++ edu-before-api/src/main/java/com/jk/modelapi/UserBean.java
@@ -21,6 +21,26 @@ public class UserBean implements Serializable {
 
     private String imgVerify;
 
+    private String status;
+
+    private String photo;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public String getId() {
         return id;
     }
